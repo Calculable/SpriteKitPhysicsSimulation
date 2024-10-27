1fad78b74cecfd8d2b5f4712ce2d3386514f10f8
Jan Huber <huber@ubique.ch>
2024-10-27 19:40:19 +0100

    Initial Commit

---
+++ SpriteKitPhysicsSimulation/SpriteKitPhysicsSimulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteKitPhysicsSimulationApp.swift
+//  SpriteKitPhysicsSimulation
+//
+//  Created by Jan Huber on 27.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SpriteKitPhysicsSimulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
